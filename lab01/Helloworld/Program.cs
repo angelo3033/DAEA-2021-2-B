708388116632614ec14e8cc1d979a7f79b6d2bd9
angel samanez <angel.samanez@tecsup.edu.pe>
2021-08-21 12:47:38 -0500

    lab01, parte 1

---
+++ lab01/Helloworld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola, como te llamas: ");
+            string nombre = Console.ReadLine(); 
+            Console.WriteLine("mucho gusto conocerte " + nombre);
+        }
+    }
+}
