c7552edb0f3678a7c578f103b9b91c5f172989ac
angel samanez <angel.samanez@tecsup.edu.pe>
2021-10-09 13:16:05 -0500

    Laboratorio 6

---
+++ Lab06/Lab06/ManPerson.Designer.cs
@@ -0,0 +1,358 @@
+﻿
+namespace Lab06
+{
+    partial class ManPerson
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvListado = new System.Windows.Forms.DataGridView();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtEnrollmentDate = new System.Windows.Forms.DateTimePicker();
+            this.txtHireDate = new System.Windows.Forms.DateTimePicker();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtPersonID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtON = new System.Windows.Forms.Button();
+            this.txtOC = new System.Windows.Forms.Button();
+            this.rdNombre = new System.Windows.Forms.RadioButton();
+            this.rdApellido = new System.Windows.Forms.RadioButton();
+            this.rdCodigo = new System.Windows.Forms.RadioButton();
+            this.rdContrato = new System.Windows.Forms.RadioButton();
+            this.rdInscripcion = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListado)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvListado
+            // 
+            this.dgvListado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListado.Location = new System.Drawing.Point(11, 367);
+            this.dgvListado.Name = "dgvListado";
+            this.dgvListado.ReadOnly = true;
+            this.dgvListado.RowHeadersVisible = false;
+            this.dgvListado.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvListado.Size = new System.Drawing.Size(521, 171);
+            this.dgvListado.TabIndex = 18;
+            this.dgvListado.SelectionChanged += new System.EventHandler(this.dgvListado_SelectionChanged);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(421, 74);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 17;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(421, 103);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 16;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(421, 43);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(75, 23);
+            this.btnInsertar.TabIndex = 15;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(217, 292);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 35);
+            this.btnBuscar.TabIndex = 14;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(421, 12);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(75, 23);
+            this.btnListar.TabIndex = 13;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtEnrollmentDate);
+            this.groupBox1.Controls.Add(this.txtHireDate);
+            this.groupBox1.Controls.Add(this.txtLastName);
+            this.groupBox1.Controls.Add(this.txtFirstName);
+            this.groupBox1.Controls.Add(this.txtPersonID);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(381, 232);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Persona";
+            // 
+            // txtEnrollmentDate
+            // 
+            this.txtEnrollmentDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.txtEnrollmentDate.Location = new System.Drawing.Point(184, 191);
+            this.txtEnrollmentDate.Name = "txtEnrollmentDate";
+            this.txtEnrollmentDate.ShowCheckBox = true;
+            this.txtEnrollmentDate.Size = new System.Drawing.Size(172, 20);
+            this.txtEnrollmentDate.TabIndex = 9;
+            // 
+            // txtHireDate
+            // 
+            this.txtHireDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.txtHireDate.Location = new System.Drawing.Point(23, 191);
+            this.txtHireDate.Name = "txtHireDate";
+            this.txtHireDate.ShowCheckBox = true;
+            this.txtHireDate.Size = new System.Drawing.Size(115, 20);
+            this.txtHireDate.TabIndex = 8;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(184, 124);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(172, 20);
+            this.txtLastName.TabIndex = 7;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(23, 124);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(115, 20);
+            this.txtFirstName.TabIndex = 6;
+            // 
+            // txtPersonID
+            // 
+            this.txtPersonID.Location = new System.Drawing.Point(23, 62);
+            this.txtPersonID.Name = "txtPersonID";
+            this.txtPersonID.Size = new System.Drawing.Size(115, 20);
+            this.txtPersonID.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigo";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(181, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Inscripcion";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(181, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Apellido";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Contrato";
+            // 
+            // txtON
+            // 
+            this.txtON.Location = new System.Drawing.Point(411, 132);
+            this.txtON.Name = "txtON";
+            this.txtON.Size = new System.Drawing.Size(99, 52);
+            this.txtON.TabIndex = 19;
+            this.txtON.Text = "Ordenar por Nombre";
+            this.txtON.UseVisualStyleBackColor = true;
+            this.txtON.Click += new System.EventHandler(this.txtON_Click);
+            // 
+            // txtOC
+            // 
+            this.txtOC.Location = new System.Drawing.Point(411, 194);
+            this.txtOC.Name = "txtOC";
+            this.txtOC.Size = new System.Drawing.Size(99, 50);
+            this.txtOC.TabIndex = 20;
+            this.txtOC.Text = "Ordenar por Codigo";
+            this.txtOC.UseVisualStyleBackColor = true;
+            this.txtOC.Click += new System.EventHandler(this.txtOC_Click);
+            // 
+            // rdNombre
+            // 
+            this.rdNombre.AutoSize = true;
+            this.rdNombre.Location = new System.Drawing.Point(331, 301);
+            this.rdNombre.Name = "rdNombre";
+            this.rdNombre.Size = new System.Drawing.Size(62, 17);
+            this.rdNombre.TabIndex = 21;
+            this.rdNombre.TabStop = true;
+            this.rdNombre.Text = "Nombre";
+            this.rdNombre.UseVisualStyleBackColor = true;
+            // 
+            // rdApellido
+            // 
+            this.rdApellido.AutoSize = true;
+            this.rdApellido.Location = new System.Drawing.Point(331, 333);
+            this.rdApellido.Name = "rdApellido";
+            this.rdApellido.Size = new System.Drawing.Size(62, 17);
+            this.rdApellido.TabIndex = 22;
+            this.rdApellido.TabStop = true;
+            this.rdApellido.Text = "Apellido";
+            this.rdApellido.UseVisualStyleBackColor = true;
+            // 
+            // rdCodigo
+            // 
+            this.rdCodigo.AutoSize = true;
+            this.rdCodigo.Location = new System.Drawing.Point(331, 264);
+            this.rdCodigo.Name = "rdCodigo";
+            this.rdCodigo.Size = new System.Drawing.Size(58, 17);
+            this.rdCodigo.TabIndex = 23;
+            this.rdCodigo.TabStop = true;
+            this.rdCodigo.Text = "Codigo";
+            this.rdCodigo.UseVisualStyleBackColor = true;
+            // 
+            // rdContrato
+            // 
+            this.rdContrato.AutoSize = true;
+            this.rdContrato.Location = new System.Drawing.Point(421, 283);
+            this.rdContrato.Name = "rdContrato";
+            this.rdContrato.Size = new System.Drawing.Size(65, 17);
+            this.rdContrato.TabIndex = 24;
+            this.rdContrato.TabStop = true;
+            this.rdContrato.Text = "Contrato";
+            this.rdContrato.UseVisualStyleBackColor = true;
+            // 
+            // rdInscripcion
+            // 
+            this.rdInscripcion.AutoSize = true;
+            this.rdInscripcion.Location = new System.Drawing.Point(421, 324);
+            this.rdInscripcion.Name = "rdInscripcion";
+            this.rdInscripcion.Size = new System.Drawing.Size(76, 17);
+            this.rdInscripcion.TabIndex = 25;
+            this.rdInscripcion.TabStop = true;
+            this.rdInscripcion.Text = "Inscripcion";
+            this.rdInscripcion.UseVisualStyleBackColor = true;
+            // 
+            // ManPerson
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 550);
+            this.Controls.Add(this.rdInscripcion);
+            this.Controls.Add(this.rdContrato);
+            this.Controls.Add(this.rdCodigo);
+            this.Controls.Add(this.rdApellido);
+            this.Controls.Add(this.rdNombre);
+            this.Controls.Add(this.txtOC);
+            this.Controls.Add(this.txtON);
+            this.Controls.Add(this.dgvListado);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ManPerson";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.ManPerson_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListado)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvListado;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker txtEnrollmentDate;
+        private System.Windows.Forms.DateTimePicker txtHireDate;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtPersonID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button txtON;
+        private System.Windows.Forms.Button txtOC;
+        private System.Windows.Forms.RadioButton rdNombre;
+        private System.Windows.Forms.RadioButton rdApellido;
+        private System.Windows.Forms.RadioButton rdCodigo;
+        private System.Windows.Forms.RadioButton rdContrato;
+        private System.Windows.Forms.RadioButton rdInscripcion;
+    }
+}
+
