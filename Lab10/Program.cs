526e5ac43cb9fce738c3e62fb406650b1ff9d368
angel samanez <angel.samanez@tecsup.edu.pe>
2021-11-07 13:19:37 -0500

    primer avance mostrar id y nombres de los productos

---
+++ Lab10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Origen de los datos
+            NorthwndDataContext context = new NorthwndDataContext();
+
+            var query = from p in context.Products
+                        select p;
+
+            foreach (var prod in query)
+            {
+                Console.WriteLine("ID={0} \t Name={1}", prod.ProductID, prod.ProductName);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
